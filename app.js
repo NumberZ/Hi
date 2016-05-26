7f84c7181723a7888b1163f6014fa1d8bb3fb59e
Vinnie <215903432@qq.com>
2016-05-26 11:04:21 +0800

    简易多人聊天室

--- app.js
+++ app.js
@@ -5,26 +5,37 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
-
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+//设置端口号
+app.set('port',process.env.PORT || 5000);
+
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
 
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, './public', 'index.html'));
+});
+
+io.on('connection',function(socket){
+    socket.broadcast.emit('hi');
+    socket.on('chat message',function(msg){
+        io.emit('chat message',msg);
+    });
+    console.log('a user has connect');
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -57,4 +68,6 @@ app.use(function(err, req, res, next) {
 });
 
 
-module.exports = app;
+http.listen(app.get('port'),function(){
+    console.log('your webapp is running in port:' + app.get('port'));
+});
