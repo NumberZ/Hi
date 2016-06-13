e1aabc126670bf030209c89aadf4451c603e9c60
Vinnie <215903432@qq.com>
2016-06-13 23:45:52 +0800

    *

--- public/js/sayHi.js
+++ public/js/sayHi.js
@@ -55,7 +55,7 @@
             }
         },
         init:function(username){
-            this.socket = io.connect('http://localhost:80');
+            this.socket = io.connect('http://115.28.68.65:80');
             this.username = username;
             this.socket.on('connect',function(){
                 Materialize.toast('登录成功！',1000);
